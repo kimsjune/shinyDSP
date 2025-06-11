f75dbeefc2e4442512e6bd8df88881cdebffad69
kimsjune <skim823@uwo.ca>
2025-06-10 21:20:08 -0400

    Triny cherry-pick AGAIN

--- R/output_setup_nav_panel.R
+++ R/output_setup_nav_panel.R
@@ -11,22 +11,12 @@
         DT::renderDT(
             rv$data()$countFile %>%
                 dplyr::slice_head(n = 100) %>%
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1a01b71 (Peer review fix: enabled paging, adj. P val cutoff, removed 'turbo' and added all 115 palettes under hcl.pals())
                 # To show all 100 rows across five pages
                 DT::datatable(
                     options = list(
                         paging = TRUE
                     )
                 )
-<<<<<<< HEAD
-=======
-                DT::datatable()
->>>>>>> c11708a (Partial peer review fixes)
-=======
->>>>>>> 1a01b71 (Peer review fix: enabled paging, adj. P val cutoff, removed 'turbo' and added all 115 palettes under hcl.pals())
         )
     })  
 
@@ -40,22 +30,12 @@
         DT::renderDT(
             rv$data()$sampleAnnoFile %>%
                 dplyr::slice_head(n = 100) %>%
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1a01b71 (Peer review fix: enabled paging, adj. P val cutoff, removed 'turbo' and added all 115 palettes under hcl.pals())
                 # To show all 100 rows across five pages
                 DT::datatable(
                     options = list(
                         paging = TRUE
                     )
                 )
-<<<<<<< HEAD
-=======
-                DT::datatable()
->>>>>>> c11708a (Partial peer review fixes)
-=======
->>>>>>> 1a01b71 (Peer review fix: enabled paging, adj. P val cutoff, removed 'turbo' and added all 115 palettes under hcl.pals())
         )
     })
     
--- R/output_sidebar.R
+++ R/output_sidebar.R
@@ -172,30 +172,12 @@
     })
     
     output$selectYourPValCutoff <- shiny::renderUI({
-<<<<<<< HEAD
-<<<<<<< HEAD
         shiny::req(rv$data())
       
         shiny::numericInput(
             inputId = "sbPvalCutoff",
             label = "Adjusted P value cutoff",
             value = 0.05
-=======
-      shiny::req(rv$data())
-      
-      shiny::numericInput(
-        inputId = "pvalcutoff",
-        label = "Adjusted P value cutoff",
-        value = 0.05
->>>>>>> c11708a (Partial peer review fixes)
-=======
-        shiny::req(rv$data())
-      
-        shiny::numericInput(
-            inputId = "sbPvalCutoff",
-            label = "Adjusted P value cutoff",
-            value = 0.05
->>>>>>> 1a01b71 (Peer review fix: enabled paging, adj. P val cutoff, removed 'turbo' and added all 115 palettes under hcl.pals())
       )
     })
     
